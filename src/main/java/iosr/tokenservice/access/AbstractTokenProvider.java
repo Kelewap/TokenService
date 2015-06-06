3c0e46a2ff71298498f0d45b712d6c04e5e3bb04
Pawel Mikolajczyk <pawel.b.mikolajczyk@gmail.com>
2015-06-06 21:14:47 +0200

    Make TokenProvider thread-safe. Hopefully.

--- src/main/java/iosr/tokenservice/access/AbstractTokenProvider.java
+++ src/main/java/iosr/tokenservice/access/AbstractTokenProvider.java
@@ -1,30 +1,48 @@
 package iosr.tokenservice.access;
 
 import iosr.tokenservice.Token;
-import org.json.JSONObject;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class AbstractTokenProvider implements TokenProvider {
 
+    private final Lock lock = new ReentrantLock();
     private Token token;
 
     @Override
     public void redeemCode(String authorizationCode) throws AuthorizationException {
-        String accessToken = obtainAccessToken(authorizationCode);
-        this.token = new Token(accessToken);
+        lock.lock();
+        try {
+            String accessToken = obtainAccessToken(authorizationCode);
+            this.token = new Token(accessToken);
+        } finally {
+            lock.unlock();
+        }
     }
 
     protected abstract String obtainAccessToken(String code) throws AuthorizationException;
 
     @Override
     public void refreshToken() throws AuthorizationException {
-        //TODO: thread safety
-        this.token = new Token(getRefreshedToken());
+        lock.lock();
+        try {
+            this.token = new Token(getRefreshedToken());
+        } finally {
+            lock.unlock();
+        }
     }
 
     protected abstract String getRefreshedToken() throws AuthorizationException;
 
     @Override
     public Token getToken() {
-        return token;
+        //TODO: well is locking here even needed?
+        lock.lock();
+        try {
+            return token;
+        } finally {
+            lock.unlock();
+        }
     }
 }
